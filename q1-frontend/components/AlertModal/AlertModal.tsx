91e9d53be385c24770e3d0ddc7999d7b7819e9f3
Eric <eericli2000@gmail.com>
2023-01-15 15:06:58 +1100

    finish frontend part 2

--- q1-frontend/components/AlertModal/AlertModal.tsx
+++ q1-frontend/components/AlertModal/AlertModal.tsx
@@ -9,7 +9,19 @@ export default function AlertModal({useContents}: AlertModalProps) {
   function onSubmitEvent(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     // hint: the alert given is at (e.target as any).elements[0].value - ignore typescript being annoying
-    console.log((e.target as any)[0].value);
+    useContents((prev) => {
+      return {
+        ...prev,
+        rowContents: [
+          ...prev.rowContents,
+          {
+            alert: (e.target as any).elements[0].value,
+            status: "",
+            updates: [],
+          },
+        ],
+      };
+    });
   }
   
   return (
